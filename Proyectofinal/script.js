eb4bb3265024ba4da63b04b4eba2e39b09d6d916
pablof007289 <pablof1706@gmail.com>
2025-03-25 22:51:07 -0600

    Inicializacion de proyecto

---
+++ Proyectofinal/script.js
@@ -0,0 +1,46 @@
+const contenedor = document.getElementById("contenedor");
+
+
+let numeros = [40, 20, 60, 10, 30, 50];  
+
+
+function crearBarras() {
+  contenedor.innerHTML = "";  
+
+  numeros.forEach(num => {
+    const barra = document.createElement("div");
+    barra.classList.add("barra");
+    barra.style.height = `${num * 4}px`; 
+    barra.textContent = num;
+    contenedor.appendChild(barra);
+  });
+}
+
+
+crearBarras();
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function metodoBurbuja() {
+    const barras = document.querySelectorAll(".barra");
+  
+    for (let i = 0; i < numeros.length - 1; i++) {
+      for (let j = 0; j < numeros.length - i - 1; j++) {
+  
+        if (numeros[j] > numeros[j + 1]) {
+
+          [numeros[j], numeros[j + 1]] = [numeros[j + 1], numeros[j]];
+  
+          barras[j].style.height = `${numeros[j] * 4}px`;
+          barras[j].textContent = numeros[j];
+  
+          barras[j + 1].style.height = `${numeros[j + 1] * 4}px`;
+          barras[j + 1].textContent = numeros[j + 1];
+  
+          await delay(2000);
+        }
+      }
+    }
+  }
